3d59001a59a2f914990ff1bb6e9618a0c598bef4
CHEY777 <88870986+CHEY777@users.noreply.github.com>
2022-03-17 05:06:06 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -18,7 +18,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/29a367457d46721b59528.mp4','https://telegra.ph/file/02c8cb90510ad6fd1e02d.mp4','https://telegra.ph/file/0e633bf979e25ef592c10.mp4','https://telegra.ph/file/0fde71c91c0e8813b754f.mp4'
+            'https://telegra.ph/file/36783a395d86adbf9fcd4.mp4','https://telegra.ph/file/fe3325fb5fa3da1de4c04.mp4','https://telegra.ph/file/29a367457d46721b59528.mp4','https://telegra.ph/file/02c8cb90510ad6fd1e02d.mp4','https://telegra.ph/file/0e633bf979e25ef592c10.mp4','https://telegra.ph/file/0fde71c91c0e8813b754f.mp4'
         ]
         let well = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
@@ -160,6 +160,10 @@ ${this.client.config.prefix}Ｈelp3 [${this.client.config.prefix}h3]
 〽️~ppcouple
 💠Discription: Sends pair dp
 🧧Usage: ${this.client.config.prefix}ppcouple
+
+〽️~chess
+💠Discription: chesssssssssssssss
+🧧Usage:${this.client.config.prefix}chess
 ────────────────
 -M E D I A 
 ${this.client.config.prefix}Ｈelp4 [${this.client.config.prefix}h4]
